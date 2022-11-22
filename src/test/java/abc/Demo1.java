d7da5137397b08a04987c901ac144f433b3d46b9
shubh8101996 <shubhamshedge8@gmail.com>
2022-11-22 20:27:39 +0530

    first commit

---
+++ src/test/java/abc/Demo1.java
@@ -0,0 +1,23 @@
+package abc;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	
+	@Test
+	public void Test1() {
+		
+		System.out.println("Test 1");
+		
+	}
+	
+	
+	@Test
+	public void Test2() {
+		
+		System.out.println("Test 2");
+		
+	}
+
+}
---
+++ src/test/java/abc/Demo2.java
@@ -0,0 +1,22 @@
+package abc;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	
+	@Test
+	public void Test3() {
+		
+		System.out.println("Test 3");
+		
+	}
+	
+	
+	@Test
+	public void Test4() {
+		
+		System.out.println("Test 4");
+		
+	}
+
+}
